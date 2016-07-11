f032af8bc49e9362435d09af1cfd5e197b5748ff
Dimitri De Kerf <dimidekerf@hotmail.com>
2016-07-11 22:37:03 +0200

    Error handling + Styling

--- src/autocomplete/MyAutoComplete.ts
+++ src/autocomplete/MyAutoComplete.ts
@@ -1,4 +1,5 @@
 import IComponentOptions = angular.IComponentOptions;
+import {Observable, Observer} from "rxjs/Rx";
 import {fromScope$} from "../rx/fromScope";
 import IScope = angular.IScope;
 import {fromEvent} from "rxjs/observable/fromEvent";
@@ -17,7 +18,9 @@ export class MyAutoComplete implements IComponentOptions {
 
 export class MyAutoCompleteController {
 
-    users: User[] = [];
+    private users: User[] = [];
+    private searchInProgress: boolean;
+    private errorMessage: string = '';
 
     static $inject = ['$scope', 'UserService'];
     constructor(private $scope: IScope, private UserService: UserService) {
@@ -25,30 +28,55 @@ export class MyAutoCompleteController {
     }
 
     $onInit(): void {
-        fromScope$('$ctrl.myInput', this.$scope)
-        /* filter out undefined or no text */
+        this.initObservable();
+    }
+
+    initObservable(): void {
+        const myInputObservable = fromScope$('$ctrl.myInput', this.$scope)
             .do(()=> {
                 this.users = [];
             })
-            .filter(x => x !== undefined)
+            /* filter out undefined or no text */
+            .filter((query: string) => query !== undefined)
             /* only check when user has stopped pressing after 100 ms
              * normally you would use a shorten timespan to keep it snappy */
             .debounceTime(500)
             /*dont make a request if the text remains the same */
             .distinctUntilChanged()
-            .filter(x => x !== "")
-            .map(x => x.toLowerCase())
-            .flatMap((qry: string) =>
-                fromPromise(this.UserService.queryUsers(qry))
+            .filter((query: string) => query !== '')
+            .map((query: string) => query.toLowerCase())
+            .do(() => {
+                this.searchInProgress = true;
+                this.errorMessage = '';
+            })
+            .flatMap((query: string) =>
+                fromPromise(this.UserService.queryUsers(query))
                 //try 3 times
-                .retry(3))
+                .retry(3)
+                //Catch AJAX errors and handle them
+                .catch((error: any) => {
+                    this.handleError(error);
+                    return Observable.empty();
+                }))
             .map((response: any)=>response.data.items)
-            .do(this.showResults)
-            .subscribe();
+            .do(() => this.searchInProgress = false);
+
+        const observer: Observer<any> = {
+            next: (users: User[]) => this.showResults(users),
+            error: err => this.handleError(err),
+            complete: () => console.log('Completed')
+        }
+
+        myInputObservable.subscribe(observer);
     }
 
-    private showResults = (users: User[]): void => {
+    showResults(users: User[]): void {
         this.users = users;
+    }
+
+    handleError(error: any): void {
+        this.searchInProgress = false;
+        this.errorMessage = error.data.message;
     }
 
 }
--- src/rx/fromScope.ts
+++ src/rx/fromScope.ts
@@ -1,5 +1,6 @@
 import IScope = angular.IScope;
 import {Observable, Observer} from "rxjs/Rx";
+
 export const fromScope$ = (property: string, $scope: IScope): Observable<any> => {
     return Observable.create((observer: Observer<any>) => {
         $scope.$on("destroy", () => {
